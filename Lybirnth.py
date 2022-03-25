8fe83017d8efb42177b5a570ee278a90f4e280c4
Ogniva <101859221+Ogniva@users.noreply.github.com>
2022-03-25 10:40:06 +0200

    Laborint random

--- Lybirnth.py
+++ Lybirnth.py
@@ -1,6 +1,6 @@
 
 from hashlib import new
-
+import random
 
 map1=[
     [12,0,1,0],
@@ -39,16 +39,18 @@ map=map4
 start_position_x = 0
 start_position_y = 0
 
+can_move_rigth = 0
+can_move_left = 0
 # Steps
 
 def move(step, current_position_x, current_position_y):
     print("Current step is: ", step)
     print("Current current_position_y =",current_position_y)
     print("Current current_position_x =",current_position_x)
-
+    
     can_move_right = current_position_x <= 4 and map[current_position_y][current_position_x+1] == 0
-    can_move_bottom = current_position_y <= 4 and map[current_position_y+1][current_position_x] == 0 
     can_move_left = current_position_x >= 1 and map[current_position_y][current_position_x-1] == 0
+    can_move_bottom = current_position_y <= 4 and map[current_position_y+1][current_position_x] == 0 
 #    can_move_top = current_position_y >= 0 and map[current_position_y-1][current_position_x] == 0
 
     right_is_finish = current_position_x <= 4 and map[current_position_y][current_position_x+1] == 24
@@ -66,15 +68,19 @@ def move(step, current_position_x, current_position_y):
         print("Found finish on left")
         return False
 
+    
     if can_move_bottom:
         print("Should move down")
         return [current_position_x, current_position_y + 1]
-    if can_move_right:
-        print("Should move right")
-        return [current_position_x + 1, current_position_y]
-    if can_move_left:
-        print("Should move left")
-        return [current_position_x - 1, current_position_y]
+    while not right_is_finish or not bottom_is_finish or not left_is_finish:
+        if random.random() > 0.5:
+            if can_move_right:
+                print("Should move right")
+                return [current_position_x + 1, current_position_y]
+        elif random.random() < 0.5:
+            if can_move_left:
+                print("Should move left")
+                return [current_position_x - 1, current_position_y]
 
 
 #new_position = move(1, start_position_x, start_position_y)
@@ -91,3 +97,4 @@ new_position = move(current_move_number, start_position_x, start_position_y)
 while new_position:
     current_move_number = current_move_number + 1
     new_position = move(current_move_number, new_position[0], new_position[1])
+
